0701a5ab093508ff544bb9d1d13589f7b97f1b00
Nikolay Zenovkin <aby2sz@gmail.com>
2020-04-21 13:27:53 +0300

    Mem 138 (#17)
    * schema merger
    
    * spec normalizer test
    
    * spec normalizer test
    
    * spec normalizer test
    
    * test fixes
    
    * test fixes
    
    * test fixes
    
    * data extractor
    
    * isort
    
    * isort
    
    * id algorithm

--- meeshkan/nlp/data_extractor.py
+++ meeshkan/nlp/data_extractor.py
@@ -1,12 +1,107 @@
+import typing
+from collections import defaultdict
+from dataclasses import dataclass
+
 from http_types import HttpExchange
+from jsonpath_rw import parse
+
+from meeshkan.nlp.ids.paths import path_to_regex
+from meeshkan.nlp.spec_normalizer import DataPath
+
+
+@dataclass(frozen=True)
+class PathRecords:
+    path_args: typing.Tuple[str]
+    path_arg_values: typing.Dict[str, typing.List[str]]
+    records: typing.List[HttpExchange]
 
 
 class DataExtractor:
     def __init__(self):
         pass
 
-    def extract_data(self, entity_locations, recordings: HttpExchange):
-        return {
-            "account": [{"id": 1, "iban": "F1111"}, {"id": 2, "iban": "F1112"}],
-            "payment": [{"id": 1, "amount": 10}],
+    def group_records(
+        self, spec: typing.Dict, recordings: typing.List[HttpExchange]
+    ) -> typing.Dict[str, PathRecords]:
+        path_regexs = [
+            (pathname, *path_to_regex(pathname)) for pathname in spec["paths"].keys()
+        ]
+
+        res = {
+            pathname: PathRecords(
+                path_arg_values={param_name: [] for param_name in parameter_names},
+                records=list(),
+                path_args=parameter_names,
+            )
+            for pathname, path_regex, parameter_names in path_regexs
         }
+
+        for rec in recordings:
+            for pathname, path_regex, parameter_names in path_regexs:
+                values = self._match_to_path(path_regex, rec.request.pathname)
+                if values is not None:
+                    res[pathname].records.append(rec)
+                    for name, value in zip(parameter_names, values):
+                        res[pathname].path_arg_values[name].append(value)
+                    break
+
+        return res
+
+    def extract_data(
+        self,
+        datapaths: typing.Dict[str, typing.Iterable[DataPath]],
+        grouped_records: typing.Dict[str, PathRecords],
+    ) -> typing.Mapping[str, typing.Any]:
+        entity_values = defaultdict(list)
+        for pathname, records in grouped_records.items():
+            for record in records.records:
+                entity, values = self._match_entity(pathname, record, datapaths)
+                if entity is not None:
+                    entity_values[entity].extend(values)
+
+        return entity_values
+
+    def _match_entity(
+        self,
+        pathname: str,
+        record: HttpExchange,
+        datapaths: typing.Dict[str, typing.Iterable[DataPath]],
+    ):
+        total_values = []
+        res_entity = None
+        for entity, paths in datapaths.items():
+            for path in paths:
+                if self._match(pathname, record, path):
+                    values = self._extract_entity_values(path, record)
+                    res_entity = entity
+                    total_values.extend(values)
+
+            if res_entity is not None:
+                return res_entity, total_values
+
+        return None, []
+
+    def _match(self, pathname: str, record: HttpExchange, path: DataPath) -> bool:
+        return (
+            pathname == path.path
+            and path.method == record.request.method.value
+            and (
+                path.request
+                or record.response.statusCode == int(typing.cast(str, path.code))
+            )
+        )
+
+    def _extract_entity_values(self, path: DataPath, record: HttpExchange):
+        body = record.request.bodyAsJson if path.request else record.response.bodyAsJson
+        return [f.value for f in parse(path.schema_path).find(body)]
+
+    def _match_to_path(
+        self, path_as_regex, request_path: str
+    ) -> typing.Optional[typing.Mapping[str, typing.Any]]:
+        match = path_as_regex.match(request_path)
+
+        if match is None:
+            return None
+
+        captures = match.groups()
+        return captures
---
+++ tests/meeshkan/nlp/test_data_extractor.py
@@ -0,0 +1,223 @@
+import json
+
+from http_types import HttpExchangeBuilder
+
+from meeshkan.nlp.data_extractor import DataExtractor
+from meeshkan.nlp.spec_normalizer import DataPath
+
+
+def test_group_records():
+    spec = {
+        "paths": {"/v1/payment/{abc}": {}, "/v1/account/{abc}/transaction/{dfe}": {}}
+    }
+    exchange = (
+        {
+            "request": {
+                "method": "post",
+                "host": "api.com",
+                "pathname": "/v1/payment/my_id111",
+                "body": json.dumps({"foo": "hello", "bar": "bye", "zaz": "baz"}),
+                "query": {},
+                "protocol": "http",
+                "headers": {},
+            },
+            "response": {
+                "statusCode": 200,
+                "body": json.dumps({"message": "hello"}),
+                "headers": {},
+            },
+        },
+        {
+            "request": {
+                "method": "post",
+                "host": "api.com",
+                "pathname": "/v1/payment/my_id22",
+                "body": json.dumps({"foo": "hello", "bar": "bye", "zaz": "baz"}),
+                "query": {},
+                "protocol": "http",
+                "headers": {},
+            },
+            "response": {
+                "statusCode": 200,
+                "body": json.dumps({"message": "hello"}),
+                "headers": {},
+            },
+        },
+        {
+            "request": {
+                "method": "post",
+                "host": "api.com",
+                "pathname": "/v1/account/my_id444/transaction/my_id555",
+                "body": json.dumps({"foo": "hello", "bar": "bye", "zaz": "baz"}),
+                "query": {},
+                "protocol": "http",
+                "headers": {},
+            },
+            "response": {
+                "statusCode": 200,
+                "body": json.dumps({"message": "hello"}),
+                "headers": {},
+            },
+        },
+    )
+
+    exchange = [HttpExchangeBuilder.from_dict(x) for x in exchange]
+
+    data_extractor = DataExtractor()
+    actual = data_extractor.group_records(spec, exchange)
+
+    assert len(actual) == 2
+    assert {"abc": ["my_id111", "my_id22"]} == actual[
+        "/v1/payment/{abc}"
+    ].path_arg_values
+    assert {"abc": ["my_id444"], "dfe": ["my_id555"]} == actual[
+        "/v1/account/{abc}/transaction/{dfe}"
+    ].path_arg_values
+
+    assert ("abc",) == actual["/v1/payment/{abc}"].path_args
+    assert ("abc", "dfe") == actual["/v1/account/{abc}/transaction/{dfe}"].path_args
+
+    assert len(actual["/v1/payment/{abc}"].records) == 2
+    assert len(actual["/v1/account/{abc}/transaction/{dfe}"].records) == 1
+
+
+def test_extract_data():
+    spec = {
+        "paths": {"/v1/payment/{abc}": {}, "/v1/account/{abc}/transaction/{dfe}": {}}
+    }
+    exchange = (
+        {
+            "request": {
+                "method": "post",
+                "host": "api.com",
+                "pathname": "/v1/payment/my_id111",
+                "body": json.dumps({"message": "hello", "bar": "bye", "zaz": "baz"}),
+                "query": {},
+                "protocol": "http",
+                "headers": {},
+            },
+            "response": {
+                "statusCode": 200,
+                "body": json.dumps(
+                    {
+                        "result": {
+                            "created": {
+                                "mId": "fds5f4-5f",
+                                "message": "hello",
+                                "bar": "bye",
+                                "zaz": "baz",
+                            }
+                        }
+                    }
+                ),
+                "headers": {},
+            },
+        },
+        {
+            "request": {
+                "method": "put",
+                "host": "api.com",
+                "pathname": "/v1/payment/my_id22",
+                "body": json.dumps(
+                    {"mId": "fds5f4-5f", "foo": "hello", "bar": "bye", "zaz": "baz"}
+                ),
+                "query": {},
+                "protocol": "http",
+                "headers": {},
+            },
+            "response": {
+                "statusCode": 200,
+                "body": json.dumps(
+                    {
+                        "result": {
+                            "updated": {
+                                "mId": "fds5f4-5f",
+                                "foo": "hello",
+                                "bar": "bye",
+                                "zaz": "baz",
+                            }
+                        }
+                    }
+                ),
+                "headers": {},
+            },
+        },
+        {
+            "request": {
+                "method": "get",
+                "host": "api.com",
+                "pathname": "/v1/account/my_id444/transaction/my_id555",
+                "body": "",
+                "query": {},
+                "protocol": "http",
+                "headers": {},
+            },
+            "response": {
+                "statusCode": 200,
+                "body": json.dumps(
+                    {
+                        "results": [
+                            {
+                                "mId": "fds5f4-5f",
+                                "foo": "hello",
+                                "bar": "bye",
+                                "zaz": "baz",
+                            },
+                            {
+                                "mId": "fsdfre-5f",
+                                "foo": "bye",
+                                "bar": "hello",
+                                "zaz": "baz",
+                            },
+                        ]
+                    }
+                ),
+                "headers": {},
+            },
+        },
+    )
+
+    exchange = [HttpExchangeBuilder.from_dict(x) for x in exchange]
+
+    data_extractor = DataExtractor()
+    grouped_records = data_extractor.group_records(spec, exchange)
+
+    datapaths = {
+        "payment": [
+            DataPath(
+                path="/v1/payment/{abc}", method="post", request=True, schema_path="$"
+            ),
+            DataPath(
+                path="/v1/payment/{abc}",
+                method="post",
+                request=False,
+                code="200",
+                schema_path="$.result.created",
+            ),
+            DataPath(
+                path="/v1/payment/{abc}",
+                method="put",
+                request=False,
+                code="200",
+                schema_path="$.result.updated",
+            ),
+            DataPath(
+                path="/v1/payment/{abc}", method="put", request=True, schema_path="$"
+            ),
+        ],
+        "transaction": [
+            DataPath(
+                path="/v1/account/{abc}/transaction/{dfe}",
+                method="get",
+                request=False,
+                code="200",
+                schema_path="$.results[*]",
+            )
+        ],
+    }
+
+    actual = data_extractor.extract_data(datapaths, grouped_records)
+
+    assert len(actual) == 2
+    assert len(actual["payment"]) == 4
+    assert len(actual["transaction"]) == 2
